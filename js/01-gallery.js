bd13aba613b4331073f0f54e56611e75f607c77a
LeMichall <120519238+LeMichall@users.noreply.github.com>
2023-05-24 23:18:32 +0200

    gallery 01 finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,8 +5,9 @@ const gallery = document.querySelector(".gallery");
 console.log(galleryItems);
 
 const createGallery = () => {
+  //"virtual" array
   let tempGallery = [];
-
+  //creating each element
   galleryItems.forEach((e, i) => {
     const { preview, original, description } = galleryItems[i];
     const li = document.createElement("li");
@@ -21,14 +22,29 @@ const createGallery = () => {
     img.setAttribute("src", preview);
     img.setAttribute("data-source", original);
     img.setAttribute("alt", description);
-
+    //creating proper item
     li.appendChild(a).appendChild(img);
     tempGallery.push(li);
   });
-  console.log(tempGallery);
+  //push to real gallery
   gallery.append(...tempGallery);
 };
-createGallery();
-gallery.addEventListener("click", () => {
+
+function eventHandler(event) {
+  //blocking unwanted click behaviour
   event.preventDefault();
-});
+  if (event.target.nodeName !== "IMG") return;
+  //modal
+  const instance = basicLightbox.create(
+    `<img src ="${event.target.dataset.source}">`
+  );
+  instance.show();
+  const visible = instance.visible();
+  if (visible) {
+    document.addEventListener("keyup", ({ key }) => {
+      if (key === "Escape") instance.close();
+    });
+  }
+}
+createGallery();
+gallery.addEventListener("click", eventHandler);
